98cc94a6bfb225e128dca0c71c8191098d613bfb
irailtonreis <irailton@gmail.com>
2020-02-17 18:37:12 -0300

    Ajustes gerais

--- src/routes.js
+++ src/routes.js
@@ -3,15 +3,10 @@ const UserController = require('./controllers/UserController');
 
 const routes = new Router();
 
-
 routes.get('/users', UserController.index);
 routes.get('/users/:id', UserController.show);
 routes.post('/users', UserController.store);
 routes.put('/users/:id', UserController.update);
 routes.delete('/users/:id', UserController.delete);
-
-
-
-
 
 module.exports = routes;
