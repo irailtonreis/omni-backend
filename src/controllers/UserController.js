bf7bd5e59affa2da880cd20e1fcb6b7d32ac5a45
irailtonreis <irailton@gmail.com>
2020-02-17 18:25:58 -0300

    Adicionando métodos index, show, update e delete

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -4,38 +4,69 @@ module.exports = {
 
   async index(req, res){
    
-   
+  const users  =  await User.find();
 
+  if(users.length == 0){
+    return res.status(400).json({ message: "Não existe usuários"})
+  }
+
+  return res.status(200).json(users);
+  
   },
 
-  async show(){
+  async show(req, res){
 
+    const {id, nome, email, avatar, descricao} = await User.findOne({id: req.query.id});
+
+    return res.status(200).json({id, nome, email, avatar, descricao});
+  
   },
 
   async store(req, res){
-    const { nome, email, avatar, descricao} = req.body;
+   
+    const userExists = await User.findOne({ email: req.body.email });
 
-    let user = await User.find({email});
-
-    if(!user){
-      user = await User.create({
-        nome,
-        email,
-        avatar,
-        descricao,
-      });
-
+    if(userExists){
+      return res.status(400).json({ error: 'Usuário já existe.'})
     }
 
-    return res.json(user);
+  const { id, nome, email, avatar, descricao }  =  await User.create(req.body);
+
+  return res.status(200).json({ id, nome, email, avatar, descricao});
 
   },
 
-  async update(){
+  async update(req, res){
+
+    const { nome, email, avatar, descricao } = req.body;
+
+    let user = await User.findOne({_id: req.params.id});
+  
+    if(!user){
+      return res.status(400).json({ message: 'Usuário não encontrado'}) 
+    }
+
+    user.nome = nome;
+    user.email = email;
+    user.avatar = avatar;
+    user.descricao = descricao;
+
+    await user.save();
+    
+    return res.json(req.body);
 
   },
 
-  async delete(){
+  async delete(req, res){
+
+    const user =  await User.findOne({_id: req.params.id});
+
+    if(user){
+      await User.deleteOne({_id: req.params.id})
+      return res.status(200).json({ message: 'Usuário deletado com sucesso!'});
+    }
+
+    return res.json({ errror: 'Usuário não encontrado!'});
 
   }
 
--- src/routes.js
+++ src/routes.js
@@ -5,7 +5,13 @@ const routes = new Router();
 
 
 routes.get('/users', UserController.index);
+routes.get('/users/:id', UserController.show);
 routes.post('/users', UserController.store);
+routes.put('/users/:id', UserController.update);
+routes.delete('/users/:id', UserController.delete);
+
+
+
 
 
 module.exports = routes;
